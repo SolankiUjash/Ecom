0dc789a51550afcbb7cb35f052b76de05a2968ac
Ujash Solanki <solankiujash280204@gmail.com>
2024-07-09 16:30:32 +0530

    Single Product Model and Product Sorting Done

--- E_shop/views.py
+++ E_shop/views.py
@@ -25,7 +25,14 @@ def PRODUCT(request):
     PRICE_FILTER_ID = request.GET.get('filter_price')
     COLOR_ID = request.GET.get('color')
     BRAND_ID = request.GET.get('brand')
-    
+    ATOZ = request.GET.get('ATOZ')
+    ZTOA = request.GET.get('ZTOA')
+    PRICE_ASC = request.GET.get('price_asc')
+    PRICE_DESC = request.GET.get('price_desc')
+    DATE_ASC = request.GET.get('date_asc')
+    DATE_DESC = request.GET.get('date_desc')
+
+
     if CATID:
         product = product.filter(categories = CATID)
         cproduct  =  product.count()
@@ -38,6 +45,18 @@ def PRODUCT(request):
     elif BRAND_ID: 
         product = product.filter(brand = BRAND_ID)
         cproduct  =  product.count()   
+    elif ATOZ:
+        product = product.order_by('name')
+    elif ZTOA:
+        product = product.order_by('-name')
+    elif PRICE_ASC:
+        product = product.order_by('price')
+    elif PRICE_DESC:
+        product = product.order_by('-price')
+    elif DATE_ASC:
+        product = product.order_by('created_date')
+    elif DATE_DESC:
+        product = product.order_by('-created_date')
     else:
         product = Product.objects.all().filter(status = "PUBLISH")    
 
